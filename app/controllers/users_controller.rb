cd22f360e9e69ad59c1c9135cb787c8e5d9f6150
olegprin <in@ex.com>
2015-03-17 10:14:52 +0200

    9 part

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,65 @@
 class UsersController < ApplicationController
-  
+
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+   def index
+    @users = User.paginate(page: params[:page])
+  end
+   def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
   def new
   	@user=User.new
   end
-
+  def edit
+   # @user = User.find(params[:id])
+  end
   def show
     @user = User.find(params[:id])
   end
- 
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
+    @user.save
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
-
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
   private
 
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 
+    # Before filters
+
+     
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
