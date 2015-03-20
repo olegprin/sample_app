93e3f1cfbd6e87ed3105772155bff0167cf0b2f2
olegprin <in@ex.com>
2015-03-20 21:09:38 +0200

    Finish demo app

---
+++ app/assets/javascripts/sitewide/home.js
@@ -0,0 +1,152 @@
+<head>
+<title>Portfoliobox - Your online portfolio website</title>
+<meta name="description" content="Portfoliobox is a web tool you can use to create your own online portfolio website."/>
+<link href='http://fonts.googleapis.com/css?family=Merriweather|Nunito:300' rel='stylesheet' type='text/css'>
+<meta charset="utf-8"/><script type="text/javascript">window.NREUM||(NREUM={}),__nr_require=function(n,t,e){function r(e){if(!t[e]){var o=t[e]={exports:{}};n[e][0].call(o.exports,function(t){var o=n[e][1][t];return r(o?o:t)},o,o.exports)}return t[e].exports}if("function"==typeof __nr_require)return __nr_require;for(var o=0;o<e.length;o++)r(e[o]);return r}({QJf3ax:[function(n,t){function e(n){function t(t,e,a){n&&n(t,e,a),a||(a={});for(var u=c(t),f=u.length,s=i(a,o,r),p=0;f>p;p++)u[p].apply(s,e);return s}function a(n,t){f[n]=c(n).concat(t)}function c(n){return f[n]||[]}function u(){return e(t)}var f={};return{on:a,emit:t,create:u,listeners:c,_events:f}}function r(){return{}}var o="nr@context",i=n("gos");t.exports=e()},{gos:"7eSDFh"}],ee:[function(n,t){t.exports=n("QJf3ax")},{}],gos:[function(n,t){t.exports=n("7eSDFh")},{}],"7eSDFh":[function(n,t){function e(n,t,e){if(r.call(n,t))return n[t];var o=e();if(Object.defineProperty&&Object.keys)try{return Object.defineProperty(n,t,{value:o,writable:!0,enumerable:!1}),o}catch(i){}return n[t]=o,o}var r=Object.prototype.hasOwnProperty;t.exports=e},{}],D5DuLP:[function(n,t){function e(n,t,e){return r.listeners(n).length?r.emit(n,t,e):(o[n]||(o[n]=[]),void o[n].push(t))}var r=n("ee").create(),o={};t.exports=e,e.ee=r,r.q=o},{ee:"QJf3ax"}],handle:[function(n,t){t.exports=n("D5DuLP")},{}],XL7HBI:[function(n,t){function e(n){var t=typeof n;return!n||"object"!==t&&"function"!==t?-1:n===window?0:i(n,o,function(){return r++})}var r=1,o="nr@id",i=n("gos");t.exports=e},{gos:"7eSDFh"}],id:[function(n,t){t.exports=n("XL7HBI")},{}],loader:[function(n,t){t.exports=n("G9z0Bl")},{}],G9z0Bl:[function(n,t){function e(){var n=v.info=NREUM.info;if(c(d,function(t,e){t in n||(n[t]=e)}),n&&n.agent&&n.licenseKey&&n.applicationID&&f&&f.body){v.proto="https"===l.split(":")[0]||n.sslForHttp?"https://":"http://",a("mark",["onload",i()]);var t=f.createElement("script");t.src=v.proto+n.agent,f.body.appendChild(t)}}function r(){"complete"===f.readyState&&o()}function o(){a("mark",["domContent",i()])}function i(){return(new Date).getTime()}var a=n("handle"),c=n(1),u=window,f=u.document,s="addEventListener",p="attachEvent",l=(""+location).split("?")[0],d={beacon:"bam.nr-data.net",errorBeacon:"bam.nr-data.net",agent:"js-agent.newrelic.com/nr-488.min.js"},v=t.exports={offset:i(),origin:l,features:{}};f[s]?(f[s]("DOMContentLoaded",o,!1),u[s]("load",e,!1)):(f[p]("onreadystatechange",r),u[p]("onload",e)),a("mark",["firstbyte",i()])},{1:11,handle:"D5DuLP"}],11:[function(n,t){function e(n,t){var e=[],o="",i=0;for(o in n)r.call(n,o)&&(e[i]=t(o,n[o]),i+=1);return e}var r=Object.prototype.hasOwnProperty;t.exports=e},{}]},{},["G9z0Bl"]);</script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="generator" content="www.Portfoliobox.net"/>
+<meta name="author" content="www.Portfoliobox.net"/>
+<meta name="keywords" content="Portfoliobox, online portfolio, skapa en hemsida">
+<meta name="google-site-verification" content="4YpVPjK1NU3MpiEgpyTkLNUg-bR7wesIzbvGSLy5_L8"/>
+<link href="http://d1qxsigluyuaz5.cloudfront.net/application/third_party/css/reset_front.css" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" type="text/css" href="http://d1qxsigluyuaz5.cloudfront.net/application/views/saleplatform/saleplatform.116.min.css">
+<script src="http://d1qxsigluyuaz5.cloudfront.net/application/third_party/js/jquery-1.8.0.min.js"></script>
+<script src="http://d1qxsigluyuaz5.cloudfront.net/application/third_party/js/jquery-extensions.js"></script>
+<script src="http://d1qxsigluyuaz5.cloudfront.net/application/views/saleplatform/saleplatform.104.min.js"></script>
+
+
+
+
+
+
+</div>
+<div id="menuShadow">&nbsp;</div>
+<section id="indexMenu">
+
+
+<img src="http://charterforcompassion.org/sites/default/files/images/ideas-that-changed-the-world-sermon-series.gif" alt="Chat - online website" id="indexImg">
+
+
+<div id="indexMenuText">
+<div id="indexLogo">
+<img src="http://d1qxsigluyuaz5.cloudfront.net/application/_img/saleplatform/medium/other/portfoliobox-logot.png" alt="Chat - online chat website"><br/>
+<h3>Create your own page</h3>
+</div>
+
+
+
+<h1 id="indexH1">
+Chat is a web tool where you can create your own twitts on the website. </h1>
+
+
+
+
+
+
+
+
+<div id="startNowButton">
+<div id="startNowButton">
+<a class="button1" href="/signup">Sign_up on chat</a>
+</div>
+</div>
+
+
+
+
+<script>
+        $(function(){
+            $("#langButton").toggle(function(){
+                $("#flagselector").fadeIn(500);
+            },function(){
+                $("#flagselector").fadeOut(500);
+            })
+        })
+    </script>
+</section>
+<script>
+    $(function(){
+
+        var topSection = {
+            indexMenuSection : $("#indexMenu"),
+            indexMenuImg : $("#indexImg"),
+            indexMenuText: $("#indexMenuText"),
+            indexMenuBorder: $("#menuBorder"),
+            indexMenuShadow: $("#menuShadow"),
+            startNowButton: $("#startNowButton"),
+            indexLogo: $("#indexLogo"),
+            topSectionRatio: 2.444,
+            textHeight : 0,
+            indexSectionStartHeight : 0,
+            isInBorderMode : false,
+
+            positionIndexSection : function(){
+
+                // Height
+                var screenWidth = $(document).width();
+
+                if(screenWidth<800){
+                    return false;
+                }
+                else {
+                    topSection.indexSectionStartHeight = screenWidth/topSection.topSectionRatio;
+                    topSection.indexMenuSection.height(topSection.indexSectionStartHeight);
+
+                    // Padding top
+                    topSection.textHeight = topSection.indexMenuText.height();
+                    var paddingTop = (topSection.indexSectionStartHeight - topSection.textHeight)/2;
+                    topSection.indexMenuText.css("padding-top",paddingTop+"px");
+                }
+
+            }
+        };
+
+        topSection.positionIndexSection();
+
+        $(window).resize(function(){
+            topSection.positionIndexSection();
+        });
+
+        $(document).scroll(function() {
+
+            var screenWidth = $(document).width();
+
+            if(screenWidth<800){
+                return false;
+            }
+            else {
+                var topPos = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+                var breakPosition = topSection.indexSectionStartHeight/2;
+                var menuBreakPosition = breakPosition-62/2;
+
+
+                if(topPos>menuBreakPosition){
+                    topSection.indexMenuBorder.fadeIn(50);
+                    topSection.indexMenuShadow.fadeOut(400);
+                }
+                else {
+                    topSection.indexMenuBorder.fadeOut(50);
+                    topSection.indexMenuShadow.fadeIn(500);
+                }
+
+
+                if(topPos<menuBreakPosition-30) {
+                    var shadowOpacity = topPos/267;
+                    var buttonOpacity = 1-shadowOpacity*2/3;
+                    topSection.indexMenuShadow.css("opacity",shadowOpacity);
+                    topSection.startNowButton.css("opacity",buttonOpacity);
+                    topSection.indexLogo.css("opacity",buttonOpacity);
+                }
+
+                if(topPos<breakPosition){
+                    var newHeight = topSection.indexSectionStartHeight-topPos;
+                    topSection.indexMenuSection.height(newHeight);
+                    topSection.indexMenuImg.css("top",topPos+"px");
+                }
+            }
+
+        });
+
+    })
+</script><section id="nicheNavigation">
